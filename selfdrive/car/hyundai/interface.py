4c1c7e435c9ecede1a5c4b302571b9e9081a26f8
MrXelious <88304526+MrXelious@users.noreply.github.com>
2022-06-15 23:47:41 -0500

    .

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -50,20 +50,18 @@ class CarInterface(CarInterfaceBase):
     if lateral_control == 'INDI':
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [0.]
-      ret.lateralTuning.indi.innerLoopGainV = [3.3]
+      ret.lateralTuning.indi.innerLoopGainV = [3.1]
       ret.lateralTuning.indi.outerLoopGainBP = [0.]
-      ret.lateralTuning.indi.outerLoopGainV = [2.8]
+      ret.lateralTuning.indi.outerLoopGainV = [2.5]
       ret.lateralTuning.indi.timeConstantBP = [0.]
       ret.lateralTuning.indi.timeConstantV = [1.4]
       ret.lateralTuning.indi.actuatorEffectivenessBP = [0.]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [1.8]
+      ret.lateralTuning.indi.actuatorEffectivenessV = [2.]
     elif lateral_control == 'LQR':
       ret.lateralTuning.init('lqr')
-
       ret.lateralTuning.lqr.scale = 1600.
       ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.dcGain = 0.0025
-
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
@@ -77,7 +75,6 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.torque.kf = 1.0 / max_lat_accel
       ret.lateralTuning.torque.ki = 0.2 / max_lat_accel
       ret.lateralTuning.torque.friction = 0.0
-
       ret.lateralTuning.torque.kd = 1.0
       ret.lateralTuning.torque.steeringAngleDeadzoneDeg = 0.5
 
